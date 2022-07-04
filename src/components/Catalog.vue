2be9ea681d5e8691d28f48759e0ee52b97167bf7
MSib <msib.dev@mail.ru>
2022-07-05 00:08:07 +0500

    Начал наполнять карточки каталога

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -8,10 +8,46 @@ const { result } = storeToRefs(storeApp)
 const { setResult } = storeApp
 </script>
 <template>
-<div class="catalog-wrap">
-  <ul v-if="result?.items" class="catalog">
-    <li v-for="item in result.items" :key="item.etag" class="item">{{item.volumeInfo.title}}</li>
-  </ul>
-</div>
+  <div class="catalog-wrap">
+    <ul v-if="result?.items" class="catalog">
+      <li v-for="item in result.items" :key="item.etag" class="item">
+      <img :src="item.volumeInfo.imageLinks.thumbnail" width="240" class="item__cover" alt="">
+      <p class="item__title">{{ item.volumeInfo.title }}</p>
+      <p class="item__subtitle">{{ item.volumeInfo.subtitle }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+.catalog {
+  margin: 20px;
+  padding-left: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  gap: 20px;
+  list-style: none;
+}
+.item {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  background-color: #f4f4f4;
+  border-radius: 4px;
+  box-shadow: inset -2px -2px 12px 0 #eee, 2px 2px 6px 0 #ccc;
+}
+.item__cover {
+  width: 140px;
+}
+.item__title {
+  margin: 0;
+  padding: 0;
+}
+.item__subtitle {
+  margin: 0;
+  padding: 0;
+  font-size: 70%;
+  font-style: italic;
+}
+</style>
